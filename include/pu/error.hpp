b954700eb17c484658537fab02f5997d2f91598f
Oblivichrash <Oblivichrash@outlook.com>
2026-07-25 07:07:45 +0800

    feat(error): unified exception handling with pu::Error

--- include/pu/error.hpp
+++ include/pu/error.hpp
@@ -6,20 +6,28 @@
 
 namespace pu {
 
-class HttpError : public std::runtime_error {
- public:
-  explicit HttpError(const std::string& msg) : std::runtime_error(msg), detail_(msg) {}
-  explicit HttpError(const std::string& msg, const std::string& detail)
-      : std::runtime_error(msg), detail_(detail) {}
-  const std::string& detail() const { return detail_; }
-
- private:
-  std::string detail_;
+// 新增：项目统一异常基类
+class Error : public std::runtime_error {
+public:
+    using std::runtime_error::runtime_error;
 };
 
-class StoreError : public std::runtime_error {
- public:
-  explicit StoreError(const std::string& msg) : std::runtime_error(msg) {}
+// 令 HttpError 继承自 pu::Error 而非直接继承 std::runtime_error
+class HttpError : public Error {
+public:
+    explicit HttpError(const std::string& msg) : Error(msg), detail_(msg) {}
+    explicit HttpError(const std::string& msg, const std::string& detail)
+        : Error(msg), detail_(detail) {}
+    const std::string& detail() const { return detail_; }
+
+private:
+    std::string detail_;
+};
+
+// 令 StoreError 继承自 pu::Error
+class StoreError : public Error {
+public:
+    explicit StoreError(const std::string& msg) : Error(msg) {}
 };
 
 }  // namespace pu
